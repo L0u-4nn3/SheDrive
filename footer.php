3a3f82e6e30d7ea564d1b427f66970cd025e136a
L0u-4nn3 <louanne.fauquenoit@isfsc.be>
2024-12-18 14:38:13 +0100

    shedrive
    Co-Authored-By: tutukir <184088958+tutukir@users.noreply.github.com>

--- footer.php
+++ footer.php
@@ -1,36 +1,36 @@
 <?php wp_footer(); ?>
 
-<footer>
-<div class="footer-container">
-        <div class="footer-section">
-            <h2>CONTACT</h2>
-            <p>support@shedrive.be</p>
-        </div>
-        <div class="footer-section">
-            <h2>RESEAUX</h2>
-            <p>
-                <span class="icon instagram"></span>
-                <a href="https://www.instagram.com/shedrive_be" target="_blank">@shedrive_be</a>
-            </p>
-            <p>
-                <span class="icon facebook"></span>
-                <a href="https://www.facebook.com/shedrive_fc" target="_blank">@shedrive_fc</a>
-            </p>
-        </div>
-        <div class="footer-section">
-            <h2>EN SAVOIR PLUS</h2>
-            <ul>
-                <li><a href="#about">À propos</a></li>
-                <li><a href="#aide">Aide</a></li>
-                <li><a href="#contact">Contact</a></li>
-                <li><a href="#faq">FAQ</a></li>
-            </ul>
-        </div>
-    </div>
-<div class="copywrite">
-        <p>SheDrive &copy; 2024</p>
-</div>
-</footer>
-
-</body>
-</html>
+  <footer>
+  <div class="footer-container">
+          <div class="footer-section">
+              <h2>CONTACT</h2>
+              <p>support@shedrive.be</p>
+          </div>
+          <div class="footer-section">
+              <h2>RESEAUX</h2>
+              <p>
+                  <span class="icon instagram"></span>
+                  <a href="https://www.instagram.com/shedrive_be" target="_blank">@shedrive_be</a>
+              </p>
+              <p>
+                  <span class="icon facebook"></span>
+                  <a href="https://www.facebook.com/shedrive_fc" target="_blank">@shedrive_fc</a>
+              </p>
+          </div>
+          <div class="footer-section">
+              <h2>EN SAVOIR PLUS</h2>
+              <ul>
+                  <li><a href="#about">À propos</a></li>
+                  <li><a href="#aide">Aide</a></li>
+                  <li><a href="#contact">Contact</a></li>
+                  <li><a href="#faq">FAQ</a></li>
+              </ul>
+          </div>
+      </div>
+  <div class="copywrite">
+          <p>SheDrive &copy; 2024</p>
+  </div>
+  </footer>
+  
+  </body>
+  </html>

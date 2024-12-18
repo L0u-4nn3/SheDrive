ec4082fb3b5d0d94b5fd06685069ffb29dc6aaa3
L0u-4nn3 <louanne.fauquenoit@isfsc.be>
2024-12-18 10:40:06 +0100

    SheDrive

---
+++ index.php
@@ -0,0 +1,102 @@
+<?php get_header(); ?>
+
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/app.css">
+
+    <section class="hero">
+        <div>
+            <h1>SheDrive<br>le covoiturage entre femmes, par des femmes.</h1>
+            <button>Découvrez Plus</button>
+        </div>
+        <img src="assets/img/shedrive3.png" alt="Deux femmes dans une voiture">
+    </section>
+    <section class="section">
+        <img src="covoiturage.jpg" alt="Femmes heureuses en covoiturage" width="300">
+        <h2>SheDrive, c'est quoi ?</h2>
+        <p>SheDrive est une plateforme de covoiturage dédiée aux femmes.<br>Notre objectif ? Offrir un espace sécurisé et solidaire.</p>
+        <h2>Notre objectif</h2>
+        <p>Offrir un espace sécurisé, solidaire et pratique pour vos déplacements. Chaque membre bénéficie d’une identification vérifiée pour garantir une confiance mutuelle.</p>
+        <p>Avec SheDrive, voyagez en toute sérénité, que ce soit pour vos trajets quotidiens ou des voyages plus longs.</p>
+    </section>
+    <section class="section">
+        <h2>Le Saviez-vous ?</h2>
+        <div class="stats">
+            <div>83% des femmes déclarent avoir déjà été harcelées dans les rues.</div>
+            <div>1 femme sur 2 se sent en insécurité après une certaine heure.</div>
+            <div>89% des femmes se sentent plus en sécurité entre elles.</div>
+        </div>
+    </section>
+    <section class="section">
+        <h2>Nos Services</h2>
+        <p>Ce que nous proposons pour améliorer votre expérience de covoiturage</p>
+        <div class="services">
+            <div><img src="co-voit-sec.png" class="card-img-top" alt="covoiturage sécurisé"><h3>COVOITURAGE SÉCURISÉ</h3><p>Des trajets entre femmes pour garantir votre sécurité. Avec SheDrive, voyagez sans appréhension grâce à un système conçu pour offrir un environnement sûr.</p></div>
+            <div><img src="..." class="card-img-top" alt="..."><h3>COMMUNAUTÉ SOLIDAIRE</h3><p>Partager vos expérience avec d’autres femmes bienveillantes. Créez des liens uniques et échangez avec une communauté qui partage vos valeurs.</p></div>
+            <div><img src="..." class="card-img-top" alt="..."><h3>ENGAGEMENT BÉNÉVOLE</h3><p>Aucun échange financier, basé sur le partage et l’entraide. Ce concept valorise l’esprit d’entraide et encourage une mobilité inclusive. Un geste simple qui fait toute la différence pour d’autres femmes.</p></div>
+            <div><img src="..." class="card-img-top" alt="..."><h3>TRAJET SUR LE CHEMIN</h3><p>Les conductrices proposent des places uniquement pour les trajets qu’elles effectuent déjà. Un modèle simple et pratique, sans détour inutile. </p></div>
+        </div>
+    </section>
+    <section class="section">
+        <h2>Rejoignez She Drive en 5 étapes :</h2>
+        <div class="step">
+            <div class="circle">1</div>
+            <div class="content">
+                <h2>INSCRIPTION</h2>
+                <p>Créez un compte en remplissant un formulaire avec vos informations personnelles et en téléchargeant une pièce d'identité. Ce processus garantit un accès sécurisé.</p>
+            </div>
+        </div>
+        <div class="step">
+            <div class="circle">2</div>
+            <div class="content">
+                <h2>VÉRIFICATION D'IDENTITÉ</h2>
+                <p>L'équipe SheDrive valide votre identité en quelques heures. Une fois approuvée, vous recevez un e-mail de confirmation pour activer votre compte.</p>
+            </div>
+        </div>
+        <div class="step">
+            <div class="circle">3</div>
+            <div class="content">
+                <h2>CONNEXION</h2>
+                <p>Accédez à la plateforme avec votre e-mail et mot de passe pour commencer à utiliser nos services.</p>
+            </div>
+        </div>
+        <div class="step">
+            <div class="circle">4</div>
+            <div class="content">
+                <h2>RECHERCHER OU CRÉER UN TRAJET</h2>
+                <ul>
+                    <li>Trouvez un trajet en indiquant votre point de départ, destination, et horaire souhaité.</li>
+                    <li>Si vous êtes conductrice, proposez un trajet en partageant les détails de votre itinéraire.</li>
+                </ul>
+            </div>
+        </div>
+        <div class="step">
+            <div class="circle">5</div>
+            <div class="content">
+                <h2>ORGANISER VOTRE TRAJET</h2>
+                <p>Utilisez la messagerie intégrée pour discuter avec les autres utilisatrices et finaliser les détails du voyage.</p>
+            </div>
+        </div>
+        <div class="arrow"></div>
+    </div>
+        
+    </section>
+    <section class="section">
+        <h2>Ce que les autres femmes pensent de She Drive</h2>
+       
+    </section>
+
+    <section class="section">
+        <h2>contactez-nous dès maintenant</h2>
+        <div class="coordonnee">
+            <h5>Notre email</h5>
+            <p>support@shedrive.be
+            <br>
+            <h5>Instagram</h5>
+            <p>@shedrive_be</p>
+            <br>
+            <h5>Facebook</h5>
+            <p>@shedrive_fc</p>
+        </div>
+       
+    </section>
+
+<?php get_footer(); ?>
